4133eddb111d5f6f98986fecd48eab2430e9e7d5
nikhilt1728 <shikamaru1161@gmail.com>
2025-09-27 22:05:42 +0530

    Update stairs.cpp

--- stairs.cpp
+++ stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int climbStairs(int n, vector<int>& costs) {
         vector<int> dp(n+1, INT_MAX);
-        dp[0] = 0; 
+        dp[0] = 1; 
 
         for (int i = 0; i <= n; i++) {
             for (int step = 1; step <= 3; step++) {
